53e2e3918067c64c04435a536d52177b3cb3ea56
dnhn-design <donghoon.yi.portfolio@gmail.com>
2026-03-14 21:48:04 -0400

    basic quiz working

--- 2dTriviaGame/script/global.gd
+++ 2dTriviaGame/script/global.gd
@@ -30,6 +30,28 @@ var data = [
 		"question": "Mauritius is a part of",
 		"options": ["America", "Africa"],
 		"correct": 1
+	},
+	{
+		"question": "Penguins are found in the",
+		"options": ["Arctic", "Antarctic"],
+		"correct": 1
+	},
+	{
+		"question": "Which of these is an existing 
+		Tectonic Plate?",
+		"options": ["Eurasian Plate", "Peruvian Plate"],
+		"correct": 0
+	},
+	{
+		"question": "The Equator line passes through 
+		which of these countries?",
+		"options": ["Indonesia", "Russia"],
+		"correct": 0
+	},
+	{
+		"question": "How many time zone does the US have?",
+		"options": ["11", "19"],
+		"correct": 0
 	}
 	#keep adding more here
 ]
--- 2dTriviaGame/script/main.gd
+++ 2dTriviaGame/script/main.gd
@@ -5,7 +5,7 @@ extends Control
 @onready var question: Label = $MarginContainer/VBoxContainer/HBoxContainer/Label
 #get the button texts and question text from the scene
 
-var current = 0
+var current = 0 #start at first question
 
 func _ready():
 	update_ui()
@@ -14,8 +14,8 @@ func _ready():
 
 func update_ui():
 	# .size() tells us max number of questions
-	if current < Global.data.size():
-		var data = Global.data[current]
+	if current < Global.data.size(): #if it is not the last question
+		var data = Global.data[current] 
 		
 		#updating the texts
 		question.text = data["question"]
@@ -26,21 +26,17 @@ func update_ui():
 		question.text = "Out of Questions"
 		ans_1.hide()
 		ans_2.hide()
+		
 
 
 
-
-func _on_button_pressed(choice: int):
-	# Get the dictionary inside the current array slot
-	var current_question = Global.questions[current]
-	
-	if choice == current_question["correct"]:
-		#the global.gd has the correct index
-		print("Correct!")
-		#I think we could add scores system here
+func _on_button_pressed(index: int) -> void:
+	var correct = Global.data[current]["correct"]
+	if index == correct:
+		print("Question " + str(current+1) + ": " + "Correct!")
 	else:
-		print("Wrong.")
-
-	# Move to the next slot in the array
-	current += 1
+		print("Question " + str(current+1) + ": " + "Wrong!")
+		
+	current += 1	
 	update_ui()
+	
