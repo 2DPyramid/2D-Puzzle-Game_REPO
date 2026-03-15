dd3dad89294dedea0f4e60bc0d5701dc429a6fbd
Mahira <fmahira@sva.edu>
2026-03-15 14:23:25 -0400

    Update global.gd
    Added 31 questions to the trivia question list.
    
    We now have 40 trivia questions

--- 2dTriviaGame/script/global.gd
+++ 2dTriviaGame/script/global.gd
@@ -52,6 +52,162 @@ var data = [
 		"question": "How many time zone does the US have?",
 		"options": ["11", "19"],
 		"correct": 0
-	}
+	},
+	#Mahira-Started adding questions
+	{
+		"question": "What season does Australia experience in December?",
+		"options": ["Winter","Summer"],
+		"correct": 1
+		},
+	{
+		"question": "How many countries are there in the world today?",
+		"options": ["195","200"],
+		"correct": 0
+		},
+	{
+		"question": "“Laser” is an acronym",
+		"options": ["True","False"],
+		"correct": 0
+	},
+	{
+		"question": "Which language family does English belong to?",
+		"options": ["Romance Languages", "Germanic Languages"],
+		"correct": 1
+	},
+	{
+		"question": "Which country is home to the most timezones in the world?",
+		"options": ["France", "Russia"],
+		"correct": 0
+	},
+	{
+		"question": "What do you call a polygon with 100 sides?",
+		"options": ["Hectogon", "Centigon"],
+		"correct": 1
+	},
+	{
+		"question": "What is the 'Golden Ratio' in numerical representation?",
+		"options": ["1.618", "2.718"],
+		"correct": 0
+	},
+	{
+		"question": "Who invented Algebra and Algorithms?",
+		"options": ["Leonhard Euler", "Muhammad ibn Musa al-Khwarizmi"],
+		"correct": 1
+	},
+	{
+		"question": "The word 'Bank' and the word 'Bankruptcy' both come from an Italian word for____",
+		"options": ["A locked chest", "A wooden bench"],
+		"correct":1
+	},
+	{
+		"question": "You are statistically more likely to be killed by a falling vending machine than by what animal?",
+		"options": ["A Shark", "A Bear"],
+		"correct": 0
+	},
+	{
+		"question": "Which came first: the Orange (the fruit) or Orange (the color)?",
+		"options": ["The fruit", "The color"],
+		"correct": 0
+	},
+	{
+		"question": "What is the largest freshwater fish ever found?",
+		"options": ["Stingray","Catfish"],
+		"correct": 0
+	},
+	{
+		"question": "Certain Siphonophores can be longer than Blue whales?",
+		"options": ["True", "False"],
+		"correct": 0
+	},
+	{
+		"question": "Colorblindness is more common in:",
+		"options": ["Men", "Women"],
+		"correct": 0
+	},
+	{
+		"question": "Female platypuses sweat milk from their skin, because they don’t have nipples",
+		"options": ["True", "False"],
+		"correct": 0
+	},
+	{
+		"question": "Male seahorse is the one that carries the eggs and gives birth",
+		"option": ["True", "False"],
+		"correct": 0
+	},
+	{
+		"question": "If a one legged Octopus gives one of its legs to its mate, how many legs does the octopus now have?",
+		"options": ["7","0"],
+		"correct": 1
+	},
+	{
+		"question": "How did Pythagoras (yeah, the triangle guy) die?",
+		"options": ["He was murdered in a bean field", "No one actually knows for sure"],
+		"correct": 1
+	},
+	{
+		"question": "The Empire State Building has its own zip code",
+		"options": ["True", "False"],
+		"correct": 0
+	},
+	{
+		"question": "What is the Capital of New York State?",
+		"options": ["Albany", "New York City"],
+		"correct": 0
+	},
+	{
+		"question": "Washington Square, Madison Square, Bryant, and Union Square Parks were all formerly:",
+		"options": ["Early Settlements", "Graveyards"],
+		"correct": 1
+	},
+	{
+		"question": "Which body of water flows beneath the Brooklyn Bridge?",
+		"options": ["East River", "Delaware River"],
+		"correct": 0
+	},
+	{
+		"question": "The Original color of the Statue of Liberty was:",
+		"options": ["Green", "Reddish-Brown"],
+		"correct": 1
+	},
+	{
+		"question": "New York City was formerly known as:",
+		"options": ["New Amsterdam", "New England"],
+		"correct": 0
+	},
+	{
+		"question": "Which of these walking tours actually take place in New York?",
+		"options": ["Rat Tour", "Snake Tour"],
+		"correct": 0
+	},
+	{
+		"question": "If a company’s profits are 'Decimated,' what percentage of your unit would be killed if you were 'Decimated'?",
+		"options": ["10%", "90%"],
+		"correct": 0
+	},
+	{
+		"question": "If you call someone 'Idiotic,' you are using a word that originally had nothing to do with intelligence in Greek. What did an idiōtēs originally refer to?",
+		"options": ["A private citizen not holding public office", "A person wjo couldn't count"],
+		"correct": 0
+	},
+	{
+		"question": "The word 'Muscle' comes from the Latin musculus. If you translate it literally, what little creature are you saying is under your skin?",
+		"options": ["A little mouse", "A hard shell"],
+		"correct": 0
+	},
+	{
+		"question": "How did Bill Cipher (yeah, the triangle guy from the hit 2012 TV show Gravity Falls) die?",
+		"options": ["He didn't die, He's in Therapy", "Gun"],
+		"correct": 0
+	},
+	{
+		"question": "Which light has the highest frequency and lowest wavelength?",
+		"options": ["Violet Light", "Red Light"],
+		"correct": 0
+	},
+	{
+		"question": "Which of these are considered a 'Fish' by modern zoologists?",
+		"options": [ "Shellfish", "Seahorse" ],
+		"correct": 1
+	},
 	#keep adding more here
 ]
